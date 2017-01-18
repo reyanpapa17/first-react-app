3f07810dda25dab630445188f46e2f068e7101ad
repapa <rey-an.papa@capgemini.com>
2017-01-18 10:36:42 +0800

    KT

--- src/CommentList.js
+++ src/CommentList.js
@@ -4,7 +4,7 @@ class CommentList extends Component {
   render () {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        Hello, world! I am Rey-An.
       </div>      
     );
   }
